43f2f89da426b87b5bd6f2012b391ccf78179b26
noa <t.noa1499@gmail.com>
2025-05-16 20:54:13 +0200

    widgets.cpp frissítés, csak x

--- widgets.cpp
+++ widgets.cpp
@@ -3,28 +3,27 @@
 using namespace genv;
 
 Widget::Widget(int x, int y, int sx, int sy)
-    : _x(x), _y(y), _sx(sx), _sy(sy)
-{}
+    : _x(x), _y(y), _size_x(sx), _size_y(sy) {}
 
 Button::Button(int x, int y, int sx, int sy, const std::string& text)
-    : Widget(x,y,sx,sy), _text(text), _pressed(false)
-{}
+    : Widget(x, y, sx, sy), _text(text), _pressed(false) {}
 
 void Button::draw() {
     gout << move_to(_x, _y) << color(200,200,200);
-    gout << box(_sx, _sy);
+    gout << box(_size_x, _size_y);
     gout << color(0,0,0);
-    gout << move_to(_x+15, _y + _sy/2 - 10) << text(_text);
+    gout << move_to(_x + 15, _y + _size_y / 2 - 10) << text(_text);
 }
 
 void Button::handle(event ev) {
     if (ev.type == ev_mouse && ev.button == btn_left) {
-        if (ev.pos_x >= _x && ev.pos_x < _x + _sx &&
-            ev.pos_y >= _y && ev.pos_y < _y + _sy) {
+        if (ev.pos_x >= _x && ev.pos_x < _x + _size_x &&
+            ev.pos_y >= _y && ev.pos_y < _y + _size_y) {
             _pressed = true;
         }
     }
 }
+
 bool Button::is_pressed() const {
     return _pressed;
 }
